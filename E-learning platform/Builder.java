a93031433d5e86226a7d5bf48c4d42f4e5236c0c
Hazem-Sayed <106878827+Hazem-Sayed@users.noreply.github.com>
2022-06-04 20:26:00 +0200

    Add files via upload

---
+++ E-learning platform/Builder.java
@@ -0,0 +1,10 @@
+package Users;
+
+public interface Builder
+{
+    public void getUserType(String userType);
+    public void userType(String userType);
+    public void setName(String name);
+    public void setEmail(String email);
+    public void setPhoneNumber(String phoneNumber);
+}
